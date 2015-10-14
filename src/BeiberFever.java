34013f78bc2bc3e191e0a946241134ccc8f966ec
Mrmeower855 <Guest@wts5.attlocal.net>
2015-10-13 17:02:06 -0700

    refff

---
+++ src/BeiberFever.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class BeiberFever {
+	public static void main(String[] args) {
+		for (int g = 0; g<2;  g++) {
+		
+				
+		for (int i = 0; i < 3; i++) {
+System.out.println("Baby");
+		}
+if(g % 2 ==0){
+	System.out.println("oooh!");
+}
+else {System.out.println("noo!");
+	
+}
+
+	}
+		System.out.println("I though you'd always be a burrito! OOOOO!");
+	}
+}
+
+// Bieber Fever challenge:
+// 1: Write a program that uses a for loop to print
+// baby
+// baby
+// baby
+// oh
+// Code should only contain one “baby”, and one “oh”
+
+// /2: Every 2nd time the “oh” should be “no”
+
+// /3: Each chorus ends with “I thought you'd always be mine”.
+
---
+++ src/Dumbee.java
@@ -0,0 +1,13 @@
+
+public class Dumbee {
+	public static void main(String[] args) {
+		String p = "Potatos";
+for (int i = 1; i<9; i++) {
+if(i==4){System.out.println("Four!"); i++;}
+if(i==8){System.out.println("More!");}
+else{	System.out.println(i+" "+p);
+}
+}
+
+}
+}
